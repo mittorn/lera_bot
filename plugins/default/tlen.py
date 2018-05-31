bd8da64ec907fb496c34028822785c27678b5fc5
pid <romkaeliseev@gmail.com>
2018-05-31 13:32:02 +0300

    add_flashbacks_and_fixes

--- plugins/default/tlen.py
+++ plugins/default/tlen.py
@@ -1,5 +1,5 @@
 from PIL import Image
-if answ[1] == 'вм':
+if answ[1] == 'тлен':
 	param = (('v','5.68'),('access_token',token),('message_ids',torep))
 	ret = requests.post('https://api.vk.com/method/messages.getById',data = param).text
 	ret = json.loads(ret)
@@ -14,18 +14,18 @@ if answ[1] == 'вм':
 		ret = ret['photo_'+str(numlist)]
 		ret = requests.get(ret).content
 		open('tmp/attachment.jpg','wb').write(ret)
-		pic1 = Image.open('attach/img/vietnam.png')
+		pic1 = Image.open('attach/img/tlen.png')
 		pic2 = Image.open('tmp/attachment.jpg')
 		pic1 = pic1.resize(pic2.size)
 		pic2 = pic2.convert('RGBA')
 		pic3 = Image.alpha_composite(pic2,pic1)
-		pic3.save('tmp/vietnam.jpg')
+		pic3.save('tmp/tlen.jpg')
 		ret = requests.get('https://api.vk.com/method/photos.getMessagesUploadServer?access_token={access_token}&v=5.68'.format(access_token=token)).json()
-		with open('tmp/vietnam.jpg', 'rb') as f:
+		with open('tmp/tlen.jpg', 'rb') as f:
 			ret = requests.post(ret['response']['upload_url'], files={'file1': f}).text
 		ret = json.loads(ret)
 		ret = requests.get('https://api.vk.com/method/photos.saveMessagesPhoto?v=5.68&album_id=-3&server='+str(ret['server'])+'&photo='+ret['photo']+'&hash='+str(ret['hash'])+'&access_token='+token).text
 		ret = json.loads(ret)
 		ret = requests.get('https://api.vk.com/method/messages.send?attachment=photo'+str(ret['response'][0]['owner_id'])+'_'+str(ret['response'][0]['id'])+'&v=5.68&peer_id='+str(toho)+'&access_token='+str(token))
 	except KeyError:
-		apisay('Пикчу то вставь',toho,torep)
+		apisay('А что я обрабатывать то должна?',toho,torep)
