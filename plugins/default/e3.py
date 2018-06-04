c61a91de6f04de32dc9dba449fbc4f55ff640e36
pid <romkaeliseev@gmail.com>
2018-06-04 17:31:24 +0300

    погода_и_Е3_модуль

---
+++ plugins/default/e3.py
@@ -0,0 +1,6 @@
+if answ[1] in ['расписание']:
+	pic('e3.jpg','',toho,'')
+if answ[1] in ['трансляция']:
+	apisay(open('files/txt/e3/trans','r').read(),toho,'')
+if answ[1] in ['e3','е3']:
+	apisay(open('files/txt/e3/e3','r').read(),toho,'')
