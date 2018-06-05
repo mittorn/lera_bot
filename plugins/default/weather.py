1195caca19c44d45d3d95ceb4a63c3120f12212b
pid <romkaeliseev@gmail.com>
2018-06-05 06:12:03 +0300

    фикс_погоды

--- plugins/default/weather.py
+++ plugins/default/weather.py
@@ -3,11 +3,11 @@ if answ[1] == 'погода':
 	weather = requests.get('http://api.openweathermap.org/data/2.5/weather', params={'lang':'ru', 'units': 'metric', 'APPID': 'ef23e5397af13d705cfb244b33d04561', 'q':answ_text}).json()
 	try:
 		msg=""
-		msg+="погода в " + str(weather['sys']['country']) + "/" + str(weather["name"]) + ":\n"
-		msg+='•температура: ' + str(weather["main"]["temp"]) + '°C\n'
-		msg+='•скорость ветра: ' + str(weather["wind"]["speed"]) + 'м/с\n'
-		msg+='•влажность: ' + str(weather["main"]["humidity"]) + '%\n'
-		#msg+='•описание: ' + weather["weather"][0]["description"].encode('utf-8') + "\n";
+		msg+="Погода в " + str(weather['sys']['country']) + "/" + str(weather["name"]) + ":\n"
+		msg+='•Температура: ' + str(weather["main"]["temp"]) + '°C\n'
+		msg+='•Скорость ветра: ' + str(weather["wind"]["speed"]) + 'м/с\n'
+		msg+='•Влажность: ' + str(weather["main"]["humidity"]) + '%\n'
+		msg+='•Состояние: ' + str(weather["weather"][0]["description"]) + "\n";
 		apisay(msg,toho,torep)
 	except:
 		apisay('Я не нашла этот населённый пункт',toho,torep)
