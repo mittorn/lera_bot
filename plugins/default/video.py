e3042366018f958da40b71aa427395b5df9cdd9d
mittorn <mittorn@sibmail.com>
2018-06-05 16:51:15 +0000

    Simple video filter
    как всегда не проверял. Должно блокироваться видео, которое не выкидывает фильтр adult. Для adult потребовался бы ещё с десяток идов. коприруй строчку с идом если ещё появятся

--- plugins/default/video.py
+++ plugins/default/video.py
@@ -5,6 +5,10 @@ if answ[1] == 'видео':
 		info = ''
 		if (res['response']['count'] != 0):
 			for item in res['response']['items']:
+				if item['id'] == 170754590:
+					continue
+				if item['title'].lower().find('theync') != -1:
+					continue
 				info = info+'video'+str(item['owner_id'])+'_'+str(item['id'])+','
 			param = (('v', '5.68'), ('peer_id',toho),('access_token',token),('forward_messages',torep),('message','Видео по вашему запросу'),('attachment',info))
 			requests.post('https://api.vk.com/method/messages.send', data=param)
