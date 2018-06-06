f11a7b6e8af941c9eac130777eabdb050356627e
pid <romkaeliseev@gmail.com>
2018-06-06 09:00:42 +0300

    fix

--- plugins/default/video.py
+++ plugins/default/video.py
@@ -5,7 +5,7 @@ if answ[1] == 'видео':
 		info = ''
 		if (res['response']['count'] != 0):
 			for item in res['response']['items']:
-				if item['id'] == 170754590:
+				if item['id'] in (170754590,170631958):
 					continue
 				if item['title'].lower().find('theync') != -1:
 					continue
